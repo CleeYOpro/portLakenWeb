2260a0fefc626783cc37dd7f7db424884af4ef79
OMV0601 <omvyas.0601@gmail.com>
2026-01-15 21:54:35 -0800

    env.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Avoid Next's recursive delete hitting OneDrive reparse points.
+  cleanDistDir: false,
 }
 
 module.exports = nextConfig
